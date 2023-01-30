1e1627dea8e19505be0eee170f6fd0c80aad895e
BhagawatiDahal <85800287+BhagawatiDahal@users.noreply.github.com>
2023-01-30 08:50:46 -0600

    firstcommit

---
+++ src/mypractice/MyPractice.java
@@ -0,0 +1,12 @@
+package mypractice;
+
+public class MyPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
